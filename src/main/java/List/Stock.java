c9f9ccd14fcbf1398f5b9bcb95800e97c7c877e6
Igor <igorsu4ugov@gmail.com>
2019-09-18 00:45:54 +0300

    Commit3

--- src/main/java/List/Stock.java
+++ src/main/java/List/Stock.java
@@ -3,8 +3,7 @@ package List;
 import java.util.List;
 
 class Stock {
-/*1. Массив с объектами "Товары".
-2. Массив с наименованиями товаров (Склад)
-3. for-each вывод товаров в консоль
-*/
+private static String [] nameOfItem = new String[]{"Картоха", "Зерно", "Мука", "Вафли", "Порошок"};
+
+
 }
