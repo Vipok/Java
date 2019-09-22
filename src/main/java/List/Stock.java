6813dda1de0aaf8df050337b0d0eff779b3284f3
Igor <igorsu4ugov@gmail.com>
2019-09-23 00:03:02 +0300

    Commit

--- src/main/java/List/Stock.java
+++ src/main/java/List/Stock.java
@@ -1,9 +1,16 @@
 package List;
 
-import java.util.List;
 
 class Stock {
 private static String [] nameOfItem = new String[]{"Картоха", "Зерно", "Мука", "Вафли", "Порошок"};
+
+/*
+1. Класс Stock хранит товар.
+2. Этот класс должен реализовывать интерфейс iterable.
+3. Нужно поместить наименования товара на склад (Stock).
+4. С помощью for-each вывести наименования товара в консоль.
+5. Сделать эту реализацию для ArrayList или LinkedList.
+ */
 
 
 }
