9a5d349bc65f559ea8800a2b205e7d773d7a55f4
Igor <igorsu4ugov@gmail.com>
2019-09-26 22:04:32 +0300

    List 1.0

---
+++ src/main/java/List/Products.java
@@ -0,0 +1,20 @@
+package List;
+
+import lombok.Getter;
+//класс с параметрами
+@Getter
+class Products {
+    private String nameOfProduct;
+    private int counts;
+
+    Products(String nameOfProduct, int counts) {
+        this.nameOfProduct = nameOfProduct;
+        this.counts = counts;
+    }
+
+    @Override
+    public String toString(){
+        return "Name of item: " + getNameOfProduct()
+                + "; Count: " + getCounts();
+    }
+}
--- src/main/java/List/Stock.java
+++ src/main/java/List/Stock.java
@@ -1,16 +1,47 @@
 package List;
 
+import java.util.Iterator;
 
 class Stock {
-private static String [] nameOfItem = new String[]{"Картоха", "Зерно", "Мука", "Вафли", "Порошок"};
+    private final static int maxStockVolume = 10;
+    private static Products[] goods = new Products[maxStockVolume];
 
-/*
-1. Класс Stock хранит товар.
-2. Этот класс должен реализовывать интерфейс iterable.
-3. Нужно поместить наименования товара на склад (Stock).
-4. С помощью for-each вывести наименования товара в консоль.
-5. Сделать эту реализацию для ArrayList или LinkedList.
- */
+    //главный метод
+    public static void main(String[] args) {
+        //добавление каких-то товаров
+        Products products1 = new Products("Potato", 23);
+        addGoodsToStock(products1);
+        Products products2 = new Products("Hammer", 12);
+        addGoodsToStock(products2);
+        Products products3 = new Products("Socket", 233);
+        addGoodsToStock(products3);
+        Products products4 = new Products("Cherry", 343);
+        addGoodsToStock(products4);
+        //создаем собственное подобие на ArrayList
+        ArrayListClass<Products> stock = new ArrayListClass(goods);
+        //создаем собственный итератор
+        Iterator<Products> iterator = stock.iterator();
+        //пример работы метода set собственного массива
+        stock.set(4, new Products("Banana", 1));
+        //пример работы get собственного массива
+        System.out.println(stock.get(2) + " - example");
+        //пародия на foreach
+        try {
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next().toString());
+            }
+        } catch (NullPointerException ex) {
+            ex.getMessage();
+        }
+    }
 
-
+    //метод добавления товара в массив goods
+    private static void addGoodsToStock(Products products) {
+        for (int i = 0; i < goods.length; i++) {
+            if (goods[i] == null) {
+                goods[i] = products;
+                i = goods.length;
+            }
+        }
+    }
 }
