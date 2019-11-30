8342db6131bb753175f630612d42db8dc28417b3
Igor <igorsu4ugov@gmail.com>
2019-11-30 22:23:55 +0300

    Map 2.0

--- src/main/java/HomeWorkStreamApi/Work.java
+++ src/main/java/HomeWorkStreamApi/Work.java
@@ -1,29 +1,56 @@
 package HomeWorkStreamApi;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class Work {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(3);
-        list.add(5);
-        list.add(6);
-        list.add(1);
-        Map<Integer, Integer> count = list.stream().collect(HashMap::new, (m, c) -> {
+        String symbols = "0123456789";
+        StringBuilder randString = new StringBuilder();
+        int count = 60 + (int) (Math.random() * 42);
+        for (int i = 0; i < count; i++)
+            randString.append(symbols.charAt((int) (Math.random() * symbols.length())));
+
+        System.out.println(randString);
+        List<Character> characters = randString.chars().mapToObj(m -> (char) m).collect(Collectors.toList());
+        Map<Character, Integer> counts = characters.stream().collect(HashMap::new, (m, c) -> {
             if (m.containsKey(c))
                 m.put(c, m.get(c) + 1);
             else m.put(c, 1);
         }, HashMap::putAll);
-        for (Map.Entry<Integer, Integer> i : count.entrySet()) {
-            System.out.println("Число: " + i.getKey() + ", Количество: " + i.getValue());
+        System.out.println(characters.size());
+
+        //отладка метода процентов
+        ArrayList<Double> doubles = new ArrayList<>();
+
+
+        for (Map.Entry<Character, Integer> i : counts.entrySet()) {
+            System.out.println("Число: " + i.getKey() + ", Количество: " + i.getValue() + ", процент: " +
+                    calculationOfPercent(i.getValue().doubleValue(), Double.parseDouble(String.valueOf(characters.size()))));
+
+            //отладка метода процентов
+          doubles.add(calculationOfPercent(i.getValue().doubleValue(), Double.parseDouble(String.valueOf(characters.size()))));
         }
+
+
+        //отладка метода процентов
+        double d = 0.0;
+        for (int i = 0; i < doubles.size(); i++){
+            d = d + doubles.get(i);
+        }
+        System.out.println("\n" + d);
+        //конец отладки
+    }
+
+    private static double calculationOfPercent(Double value, Double size) {
+        return value * 100/size;
     }
 }
